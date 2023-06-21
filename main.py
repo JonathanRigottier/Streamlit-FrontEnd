9529581b13588dc33fcc40c715361cb64c47d948
Jonathan Rigottier <jrigottier@gmail.com>
2023-06-21 06:08:04 +0300

    'New_graphs_added'

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ import pandas as pd
 
 header = st.container()
 dataset = st.container()
-features = st.container()
-model_training = st.container()
 
 with header:
 	st.title('Welcome to my awesome data science project!')
-	st.text('In this project I look into the transactions of taxis in NYC. ...')
+	st.text('In this project I look into the titanic passengers. ...')
 
 
 with dataset:
-	st.header('NYC taxi dataset')
-	st.text('I found this dataset on blablable.com, ...')
+	st.header('Titanic dataset')
+	st.text('I found this dataset on a free course, ...')
 
 	titanic = pd.read_csv('Data/titanic.csv')
 	st.write(titanic.head())
@@ -22,25 +20,15 @@ with dataset:
 	age = pd.DataFrame(titanic['age'].value_counts())
 	st.bar_chart(age)
 
+	st.subheader('Class of the titanic passenger')
+	pclass = pd.DataFrame(titanic['pclass'].value_counts())
+	st.bar_chart(pclass)
 
-with features:
-	st.header('The features I created')
+	st.subheader('Fare of the titanic passenger')
+	fare = pd.DataFrame(titanic['fare'].value_counts())
+	st.bar_chart(fare)
 
-	st.markdown('* **first feature** I created this feature to calculate age')
 
 
-with model_training:
-	st.header('Time to train the model!')
-	st.text('Here you can get what you want')
-
-	sel_col, disp_col = st.columns(2)
-
-	user_input = sel_col.slider('What should be the max_depth of the model?', 
-		min_value=0, max_value=150, value=10, step=10)
-	
-	n_estimators = sel_col.selectbox('How many trees should there be?', options=[100,200,300,'No limit'], 
-		index = 0)
-
-	input_feature = sel_col.text_input('which feature should be used as the input feature?','PU')
 
 
